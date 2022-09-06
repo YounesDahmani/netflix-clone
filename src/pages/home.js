af6c8b02c3f2aafc5531153ac13e7671af480a11
Younes Dahmani <y.dahmaniyounes@gmail.com>
2022-09-06 10:19:42 +0200

    Realization of the header for the Home page.

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+import { HeaderContainer } from "../containers/header";
+import { Feature, OptForm } from "../components";
 import { JumbotronContainer } from "../containers/jumbotron";
 import { FaqsContainer } from "../containers/faqs";
 import { FooterContainer } from "../containers/footer";
@@ -7,6 +8,26 @@ import { FooterContainer } from "../containers/footer";
 export default function Home() {
   return (
     <>
+      <HeaderContainer>
+        <Feature>
+          <Feature.Title>
+            Unlimited films, TV programmes and more.
+          </Feature.Title>
+          <Feature.Subtitle>
+            Watch anywhere. Cancel at any time.
+          </Feature.Subtitle>
+          <OptForm>
+            <OptForm.Input placeholder="Email address" />
+            <OptForm.Button>Try it Now</OptForm.Button>
+            <OptForm.Break />
+            <OptForm.Text>
+              Ready to watch? Enter your email to create or restart your
+              membership
+            </OptForm.Text>
+          </OptForm>
+        </Feature>
+      </HeaderContainer>
+
       <JumbotronContainer />
       <FaqsContainer />
       <FooterContainer />
