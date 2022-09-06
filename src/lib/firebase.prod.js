324fa291a4932516fed070bc99b34514117e121e
Younes Dahmani <y.dahmaniyounes@gmail.com>
2022-09-06 14:35:52 +0200

    pause

---
+++ src/lib/firebase.prod.js
@@ -0,0 +1,17 @@
+// import firebase from "firebase/compat/app";
+// import { getFirestore } from "@firebase/firestore"
+// import "firebase/auth";
+// import { seedDatabase } from "../seed";
+
+// const config = firebase.initializeApp({
+//   apiKey: "AIzaSyAwGo8QQoXVtJtPMgyNBUarVqfSv6Fx32M",
+//   authDomain: "netflix-clone-cab6a.firebaseapp.com",
+//   projectId: "netflix-clone-cab6a",
+//   storageBucket: "netflix-clone-cab6a.appspot.com",
+//   messagingSenderId: "65116085343",
+//   appId: "1:65116085343:web:5101b866821d8ed3c1cfea",
+// });
+
+// seedDatabase(firebase);
+
+// export { firebase };
